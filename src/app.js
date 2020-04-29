6ec732d6c49bb7f98af8467e5c70b25ac1d8d22d
Tim Douglas <me@timdouglas.net>
2020-04-29 09:30:08 +0100

    finished the refactor and boy is it way easier to see what's happening

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const stringsRouter = require('./routers/stringsRouter');
 const numbersRouter = require('./routers/numbersRouter');
 const booleansRouter = require('./routers/booleansRouter');
-const arrays = require('./lib/arrays');
-
-const success = 200;
-const fail = 400;
+const arraysRouter = require('./routers/arraysRouter');
 
 const app = express();
 
 app.use(express.json());
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use('/strings', stringsRouter);
 
@@ -20,37 +14,6 @@ app.use('/numbers', numbersRouter);
 
 app.use('/booleans', booleansRouter);
 
-app.post('/arrays/element-at-index/:param1', (req, res) => {
-  const { array } = req.body;
-  const index = req.params.param1;
-
-  res.status(success).json({ result: arrays.getNthElement(index, array) });
-});
-
-app.post('/arrays/to-string', (req, res) => {
-  const { array } = req.body;
-
-  res.status(success).json({ result: arrays.arrayToCSVString(array) });
-});
-
-app.post('/arrays/append', (req, res) => {
-  const { array } = req.body;
-  const { value } = req.body;
-
-  res.status(success).json({ result: arrays.addToArray2(value, array) });
-});
-
-app.post('/arrays/starts-with-vowel', (req, res) => {
-  const { array } = req.body;
-
-  res.status(success).json({ result: arrays.elementsStartingWithAVowel(array) });
-});
-
-app.post('/arrays/remove-element', (req, res) => {
-  const { array } = req.body;
-  const { index } = req.query || 0;
-
-  res.status(success).json({ result: arrays.removeNthElement2(index, array) });
-});
+app.use('/arrays', arraysRouter);
 
 module.exports = app;
